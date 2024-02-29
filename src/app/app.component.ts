d9504acf11ecc131975c2d2de0d2aea92260d2a3
Milan.Kiffmeyer <mkiffmeyer@gmx.de>
2024-02-29 12:45:23 +0100

    course-template.component.html strukturell erstellt @angular/material: ^17.2.1 zu den dependencies hinzugefügt.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CourseTemplateComponent} from "./components/template/course-template/course-template.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseTemplateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
