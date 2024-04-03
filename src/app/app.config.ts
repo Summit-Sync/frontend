48a50fdfebed7b4afd96311720f6a40cbb741369
Leon Hefling <hefling@informationsgesellschaft.com>
2024-04-03 14:52:54 +0200

    de localization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import localeDe from '@angular/common/locales/de';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeDe);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync('noop')]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync('noop'),
+    { provide: LOCALE_ID, useValue: 'de-DE' },
+  ],
 };
