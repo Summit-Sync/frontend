09f11bdd1d1ad3c0f8bd139dc78c5b3fb17bb94c
Leon Hefling <hefling@informationsgesellschaft.com>
2024-04-03 14:56:27 +0200

    course view cache

--- src/app/components/final/course/course.component.ts
+++ src/app/components/final/course/course.component.ts
@@ -1,22 +1,16 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  numberAttribute,
-} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CourseService } from '../../../services/course/course.service';
 import { Course } from '../../../models/Course';
-import { CommonModule } from '@angular/common';
-import { DateConverterService } from '../../../services/dateConverter/date-converter.service';
+import { CommonModule, formatDate } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Participant } from '../../../models/Participant';
+import { EndTimePipe } from '../../../pipes/end-time.pipe';
+import { Price } from '../../../models/Price';
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, EndTimePipe],
   templateUrl: './course.component.html',
   styleUrl: './course.component.scss',
 })
@@ -24,17 +18,12 @@ export class CourseComponent implements OnInit {
   @Output() close = new EventEmitter();
   @Input() isEdit: boolean = false;
   @Input() isDelete: boolean = false;
-  courseDetails: Course;
-  courseEditData: Course;
-  participantAddedCount: number = 0;
+  courseData: Course;
 
   // MatDialog on hold for now
   // public dialogRef: MatDialogRef<CourseComponent>
 
-  constructor(
-    public courseService: CourseService,
-    public dateConverterService: DateConverterService
-  ) {}
+  constructor(public courseService: CourseService) {}
 
   ngOnInit(): void {
     this.courseService.currentCourse.subscribe((c) => {
@@ -42,59 +31,78 @@ export class CourseComponent implements OnInit {
         console.error('no course to show');
         return;
       }
-      this.courseDetails = c;
+
       if (this.isEdit) {
-        this.courseEditData = new Course(
-          c.id,
-          c.courseTitle,
-          c.courseAbbreviation,
-          c.courseNumber,
-          c.description,
-          c.datesCount,
-          c.dates,
-          c.duration,
-          c.participants,
-          c.waitList,
-          c.maxParticipants,
-          c.maxWaitingListLength,
-          c.priceList,
-          c.place,
-          c.trainerQualifications,
-          c.trainers,
-          c.notes,
-          c.visible,
-          c.canceled
+        this.courseData = structuredClone(c);
+        console.log(this.courseData);
+        // this.fillDatesList();
+        this.fillParticipantsList(
+          this.courseData.participants,
+          this.courseData.maxParticipants
         );
+        return;
       }
+      this.courseData = c;
+      this.fillParticipantsList(
+        this.courseData.participants,
+        this.courseData.maxParticipants
+      );
     });
   }
 
   addDate() {
-    console.log('date added');
+    this.courseData.dates.push(new Date());
   }
 
   deleteDate(index: number): void {
-    this.courseEditData.dates.splice(index, 1);
+    this.courseData.dates.splice(index, 1);
+  }
+
+  onDateChange(event: Event, index: number) {
+    const inputElement = event.target as HTMLInputElement;
+    const newDate = inputElement.valueAsDate;
+    if (newDate) {
+      this.courseData.dates[index] = newDate;
+    }
+  }
+
+  onStartTimeChange(event: Event, index: number) {
+    const inputElement = event.target as HTMLInputElement;
+    const timeMS = inputElement.valueAsNumber;
+    this.courseData.dates[index].setHours(timeMS / 3600000);
+    this.courseData.dates[index].setMinutes((timeMS % 3600000) / 60000);
+    console.log(this.courseData.dates[index]);
+  }
+
+  onEndTimeChange(event: Event, index: number) {
+    const inputElement = event.target as HTMLInputElement;
+    const timeMS = inputElement.valueAsNumber;
+    this.courseData.dates[index].setHours(timeMS / 3600000);
+    this.courseData.dates[index].setMinutes((timeMS % 3600000) / 60000);
+  }
+
+  getEndTime(currentTime: Date): string {
+    return currentTime.getHours() + 8 + ':' + currentTime.getMinutes();
   }
 
   addPrice() {
-    console.log('price added');
+    this.courseData.priceList.push(new Price('', 0));
   }
 
-  deleteParticipant(index: number): void {
-    this.courseEditData.participants.splice(index, 1);
+  deletePrice(index: number) {
+    this.courseData.priceList.splice(index, 1);
   }
 
   save(): void {
-    if (this.checkUnfinishedParticipants(this.courseEditData.participants)) {
-      this.deleteEmptyParticipants(this.courseEditData.participants);
+    if (this.checkUnfinishedParticipants(this.courseData.participants)) {
+      this.deleteEmptyParticipants(this.courseData.participants);
       this.close.emit();
-      console.log('save: ', this.courseEditData);
+      console.log('save: ', this.courseData);
     }
   }
 
   cancel(): void {
-    this.deleteEmptyParticipants(this.courseDetails.participants);
+    this.deleteEmptyParticipants(this.courseData.participants);
     this.close.emit();
     console.log('cancel');
   }
@@ -104,16 +112,20 @@ export class CourseComponent implements OnInit {
     console.log('delete');
   }
 
-  fullParticipantsList(
+  onMaxParticipantsChange(): void {
+    const participantsLength = this.courseData.participants.length;
+    if (this.courseData.maxParticipants < participantsLength) {
+      this.courseData.participants.splice(participantsLength - 1, 1);
+    }
+  }
+
+  fillParticipantsList(
     participantsList: Participant[],
     maxParticipants: number
-  ) {
+  ): void {
     for (let i = participantsList.length; i < maxParticipants; i++) {
-      console.log(i);
       participantsList.push(new Participant(i, '', '', '', ''));
     }
-    console.log(participantsList);
-    return participantsList;
   }
 
   checkUnfinishedParticipants(participantsList: Participant[]): boolean {
@@ -130,7 +142,7 @@ export class CourseComponent implements OnInit {
         p.eMail != '' &&
         p.phonenumber != '' &&
         p.status != '';
-      if (allEmpty || allFilled) {
+      if (!(allEmpty || allFilled)) {
         console.error(
           'Teilnehmer ' + p.id + ' ist noch nicht fertig ausgefüllt'
         );
@@ -147,11 +159,18 @@ export class CourseComponent implements OnInit {
   }
 
   addParticipant(): void {
-    let p = this.courseEditData.participants;
-    this.courseEditData.maxParticipants++;
-    this.participantAddedCount++;
+    if (
+      this.courseData.maxParticipants <= this.courseData.participants.length
+    ) {
+      return;
+    }
+    let p = this.courseData.participants;
+    // this.courseData.maxParticipants++;
     p.push(new Participant(p.length, '', '', '', ''));
-    console.log('participant added');
+  }
+
+  deleteParticipant(index: number): void {
+    this.courseData.participants.splice(index, 1);
   }
 
   // MatDialog on hold for now
