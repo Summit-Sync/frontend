5d6778c14d749bebb964502113997e11736903a2
Leon Hefling <hefling@informationsgesellschaft.com>
2024-04-09 16:38:47 +0200

    feat(multi-select-dropdown): initialized

---
+++ src/app/pipes/checkbox/check-item-in-list.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CheckItemInListPipe } from './check-item-in-list.pipe';
+
+describe('CheckItemInListPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CheckItemInListPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
