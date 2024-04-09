0c7d96effcf968ea74b69c056f7c2c85362fdb14
Leon Hefling <hefling@informationsgesellschaft.com>
2024-04-09 16:47:55 +0200

    feat(course-component): dates view updated, trainer and qualification checkbox dropdown component added and variable names updated

--- src/app/components/final/course/course.component.ts
+++ src/app/components/final/course/course.component.ts
@@ -1,16 +1,22 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CourseService } from '../../../services/course/course.service';
 import { Course } from '../../../models/Course';
-import { CommonModule, formatDate } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Participant } from '../../../models/Participant';
-import { EndTimePipe } from '../../../pipes/end-time.pipe';
+import { EndTimePipe } from '../../../pipes/endTime/end-time.pipe';
 import { Price } from '../../../models/Price';
+import { DateTimeMapperService } from '../../../services/dateTimeMapper/date-time-mapper.service';
+import { Qualification } from '../../../models/Qualification';
+import { QualificationsService } from '../../../services/qualifications/qualifications.service';
+import { TrainerService } from '../../../services/trainer/trainer.service';
+import { Trainer } from '../../../models/Trainer';
+import { CheckItemInListPipe } from '../../../pipes/checkbox/check-item-in-list.pipe';
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [CommonModule, FormsModule, EndTimePipe],
+  imports: [CommonModule, FormsModule, EndTimePipe, CheckItemInListPipe],
   templateUrl: './course.component.html',
   styleUrl: './course.component.scss',
 })
@@ -18,12 +24,21 @@ export class CourseComponent implements OnInit {
   @Output() close = new EventEmitter();
   @Input() isEdit: boolean = false;
   @Input() isDelete: boolean = false;
+  allQualifications: Qualification[];
+  allTrainers: Trainer[];
   courseData: Course;
-
+  mappedDateTime: string[][] = [];
+  showQualificationList: boolean = false;
+  showTrainerList: boolean = false;
   // MatDialog on hold for now
   // public dialogRef: MatDialogRef<CourseComponent>
 
-  constructor(public courseService: CourseService) {}
+  constructor(
+    public courseService: CourseService,
+    public qualifcationsService: QualificationsService,
+    public trainerService: TrainerService,
+    private dateTimeMapper: DateTimeMapperService
+  ) {}
 
   ngOnInit(): void {
     this.courseService.currentCourse.subscribe((c) => {
@@ -31,31 +46,62 @@ export class CourseComponent implements OnInit {
         console.error('no course to show');
         return;
       }
-
-      if (this.isEdit) {
-        this.courseData = structuredClone(c);
-        console.log(this.courseData);
-        // this.fillDatesList();
-        this.fillParticipantsList(
-          this.courseData.participants,
-          this.courseData.maxParticipants
-        );
-        return;
-      }
-      this.courseData = c;
+      this.courseData = structuredClone(c);
+      // this.fillDatesList();
       this.fillParticipantsList(
-        this.courseData.participants,
-        this.courseData.maxParticipants
+        this.courseData.participantList,
+        this.courseData.numberParticipants
       );
+      return;
+    });
+    this.mapDateTime();
+
+    this.qualifcationsService.getAllQualifications().subscribe((q) => {
+      this.allQualifications = q;
+    });
+    this.trainerService.getAllTrainers().subscribe((t) => {
+      this.allTrainers = t;
+    });
+  }
+
+  mapDateTime() {
+    const duration = this.courseData.duration;
+    this.courseData.dates.forEach((date, index) => {
+      this.mappedDateTime[index] = [];
+      this.mappedDateTime[index][0] =
+        this.dateTimeMapper.mapDateToStartTimeString(
+          date.getHours(),
+          date.getMinutes()
+        );
+      this.mappedDateTime[index][1] =
+        this.dateTimeMapper.mapDateToEndTimeString(
+          date.getHours(),
+          date.getMinutes(),
+          duration
+        );
     });
   }
 
   addDate() {
-    this.courseData.dates.push(new Date());
+    const datesLength = this.courseData.dates.length;
+    this.mappedDateTime[datesLength] = [];
+    this.mappedDateTime[datesLength].push(
+      '12:00',
+      this.dateTimeMapper.mapDateToEndTimeString(
+        12,
+        0,
+        this.courseData.duration
+      )
+    );
+    const addedDate = new Date();
+    addedDate.setHours(12);
+    addedDate.setMinutes(0);
+    this.courseData.dates.push(addedDate);
   }
 
   deleteDate(index: number): void {
     this.courseData.dates.splice(index, 1);
+    this.mappedDateTime.splice(index, 1);
   }
 
   onDateChange(event: Event, index: number) {
@@ -68,17 +114,29 @@ export class CourseComponent implements OnInit {
 
   onStartTimeChange(event: Event, index: number) {
     const inputElement = event.target as HTMLInputElement;
+    const duration = this.courseData.duration;
     const timeMS = inputElement.valueAsNumber;
-    this.courseData.dates[index].setHours(timeMS / 3600000);
-    this.courseData.dates[index].setMinutes((timeMS % 3600000) / 60000);
-    console.log(this.courseData.dates[index]);
+    const hours = this.dateTimeMapper.convertMilisecondsToFullHours(timeMS);
+    const minutes = this.dateTimeMapper.calculateMinutesRemainder(timeMS);
+    this.courseData.dates[index].setHours(hours);
+    this.courseData.dates[index].setMinutes(minutes);
+
+    this.mappedDateTime[index][0] =
+      this.dateTimeMapper.mapDateToStartTimeString(hours, minutes);
+    this.mappedDateTime[index][1] = this.dateTimeMapper.mapDateToEndTimeString(
+      hours,
+      minutes,
+      duration
+    );
   }
 
   onEndTimeChange(event: Event, index: number) {
     const inputElement = event.target as HTMLInputElement;
     const timeMS = inputElement.valueAsNumber;
-    this.courseData.dates[index].setHours(timeMS / 3600000);
-    this.courseData.dates[index].setMinutes((timeMS % 3600000) / 60000);
+    const hours = this.dateTimeMapper.convertMilisecondsToFullHours(timeMS);
+    const minutes = this.dateTimeMapper.calculateMinutesRemainder(timeMS);
+    this.courseData.dates[index].setHours(hours);
+    this.courseData.dates[index].setMinutes(minutes);
   }
 
   getEndTime(currentTime: Date): string {
@@ -93,16 +151,72 @@ export class CourseComponent implements OnInit {
     this.courseData.priceList.splice(index, 1);
   }
 
+  deleteQualification(clickedQualification: Qualification) {
+    var tqList = this.courseData.trainerQualifications;
+    tqList = tqList.filter((q) => {
+      return q.id !== clickedQualification.id;
+    });
+
+    this.courseData.trainerQualifications = tqList;
+  }
+
+  addQualification(clickedQualification: Qualification) {
+    this.courseData.trainerQualifications.push(clickedQualification);
+  }
+
+  showQualificationsList() {
+    this.showQualificationList = !this.showQualificationList;
+    this.showTrainerList = false;
+  }
+
+  onQualificationCheckBoxClick(event: Event, index: number) {
+    const checkbox = event.target as HTMLInputElement;
+    const clickedQualification = this.allQualifications[index];
+    checkbox.checked
+      ? this.addQualification(clickedQualification)
+      : this.deleteQualification(clickedQualification);
+  }
+
+  deleteTrainer(clickedTrainers: Trainer) {
+    var tqList = this.courseData.trainers;
+    tqList = tqList.filter((q) => {
+      return q.id !== clickedTrainers.id;
+    });
+    this.courseData.trainers = tqList;
+  }
+
+  addTrainer(clickedTrainer: Trainer) {
+    this.courseData.trainers.push(clickedTrainer);
+  }
+
+  showTrainersList() {
+    this.showTrainerList = !this.showTrainerList;
+    this.showQualificationList = false;
+  }
+
+  onTrainerCheckBoxClick(event: Event, index: number) {
+    const checkbox = event.target as HTMLInputElement;
+    const clickedTrainer = this.allTrainers[index];
+    checkbox.checked
+      ? this.addTrainer(clickedTrainer)
+      : this.deleteTrainer(clickedTrainer);
+  }
+
   save(): void {
-    if (this.checkUnfinishedParticipants(this.courseData.participants)) {
-      this.deleteEmptyParticipants(this.courseData.participants);
-      this.close.emit();
+    if (
+      !this.checkUnfinishedParticipants(this.courseData.participantList) &&
+      !this.checkUnfinishedParticipants(this.courseData.waitList)
+    ) {
+      this.deleteEmptyParticipants(this.courseData.participantList);
+      this.deleteEmptyWaitingParticipants(this.courseData.waitList);
       console.log('save: ', this.courseData);
+      this.close.emit();
     }
   }
 
   cancel(): void {
-    this.deleteEmptyParticipants(this.courseData.participants);
+    this.deleteEmptyParticipants(this.courseData.participantList);
+    this.deleteEmptyWaitingParticipants(this.courseData.waitList);
     this.close.emit();
     console.log('cancel');
   }
@@ -112,10 +226,56 @@ export class CourseComponent implements OnInit {
     console.log('delete');
   }
 
+  addWaitingParticipant() {
+    if (this.courseData.numberWaitlist <= this.courseData.waitList.length) {
+      return;
+    }
+    let wp = this.courseData.waitList;
+    wp.push(new Participant(wp.length, '', '', '', '', ''));
+  }
+
+  deleteWaitingParticipant(index: number) {
+    this.courseData.waitList.splice(index, 1);
+  }
+
+  onMaxWaitlistChange() {
+    const waitlistLength = this.courseData.waitList.length;
+    if (this.courseData.numberWaitlist < waitlistLength) {
+      this.courseData.waitList.splice(waitlistLength - 1, 1);
+    }
+  }
+
+  checkUnfinishedWaitlistParticipants(): boolean {
+    console.log('waitlistParticipants');
+    const unfinishedParticipantExists = this.courseData.waitList.some((wp) => {
+      const allEmpty =
+        wp.firstname == '' &&
+        wp.lastname == '' &&
+        wp.eMail == '' &&
+        wp.phonenumber == '' &&
+        wp.status == '';
+      const allFilled =
+        wp.firstname != '' &&
+        wp.lastname != '' &&
+        wp.eMail != '' &&
+        (wp.phonenumber != '' || wp.status != '');
+      if (!(allEmpty || allFilled)) {
+        console.error(
+          `Der Teilnehmer auf der Warteliste (ID: ${wp.id}) hat unvollständige Informationen.`
+        );
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    return unfinishedParticipantExists;
+  }
+
   onMaxParticipantsChange(): void {
-    const participantsLength = this.courseData.participants.length;
-    if (this.courseData.maxParticipants < participantsLength) {
-      this.courseData.participants.splice(participantsLength - 1, 1);
+    const participantsLength = this.courseData.participantList.length;
+    if (this.courseData.numberParticipants < participantsLength) {
+      this.courseData.participantList.splice(participantsLength - 1, 1);
     }
   }
 
@@ -124,12 +284,12 @@ export class CourseComponent implements OnInit {
     maxParticipants: number
   ): void {
     for (let i = participantsList.length; i < maxParticipants; i++) {
-      participantsList.push(new Participant(i, '', '', '', ''));
+      participantsList.push(new Participant(i, '', '', '', '', ''));
     }
   }
 
-  checkUnfinishedParticipants(participantsList: Participant[]): boolean {
-    participantsList.forEach((p) => {
+  checkUnfinishedParticipants(participants: Participant[]): boolean {
+    const unfinishedParticipantExists = participants.some((p) => {
       const allEmpty =
         p.firstname == '' &&
         p.lastname == '' &&
@@ -140,37 +300,45 @@ export class CourseComponent implements OnInit {
         p.firstname != '' &&
         p.lastname != '' &&
         p.eMail != '' &&
-        p.phonenumber != '' &&
-        p.status != '';
+        (p.phonenumber != '' || p.status != '');
       if (!(allEmpty || allFilled)) {
         console.error(
           'Teilnehmer ' + p.id + ' ist noch nicht fertig ausgefüllt'
         );
+        return true;
+      } else {
         return false;
       }
     });
-    return true;
+
+    return unfinishedParticipantExists;
   }
 
   deleteEmptyParticipants(participantsList: Participant[]) {
-    participantsList.filter((p) => {
-      p.firstname != '';
+    this.courseData.participantList = participantsList.filter((p) => {
+      return p.firstname != '';
+    });
+  }
+
+  deleteEmptyWaitingParticipants(waitingParticipantsList: Participant[]) {
+    this.courseData.waitList = waitingParticipantsList.filter((wp) => {
+      return wp.firstname != '';
     });
   }
 
   addParticipant(): void {
     if (
-      this.courseData.maxParticipants <= this.courseData.participants.length
+      this.courseData.numberParticipants <=
+      this.courseData.participantList.length
     ) {
       return;
     }
-    let p = this.courseData.participants;
-    // this.courseData.maxParticipants++;
-    p.push(new Participant(p.length, '', '', '', ''));
+    let p = this.courseData.participantList;
+    p.push(new Participant(p.length, '', '', '', '', ''));
   }
 
   deleteParticipant(index: number): void {
-    this.courseData.participants.splice(index, 1);
+    this.courseData.participantList.splice(index, 1);
   }
 
   // MatDialog on hold for now
