a173ed7edebb840003d0175470790467bb806b5b
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-04-11 08:49:34 +0200

    removed duplicate service

--- src/app/services/location/location.service.spec.ts
+++ src/app/services/location/location.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { QualificationService } from './qualification.service';
+import { LocationService } from './location.service';
 
-describe('QualificationService', () => {
-  let service: QualificationService;
+describe('LocationService', () => {
+  let service: LocationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(QualificationService);
+    service = TestBed.inject(LocationService);
   });
 
   it('should be created', () => {
