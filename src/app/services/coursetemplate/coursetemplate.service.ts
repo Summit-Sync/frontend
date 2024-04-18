cd81c7ff602f9255568b79510d9f0cc751d21afa
Leon Hefling <hefling@informationsgesellschaft.com>
2024-04-18 10:58:40 +0200

    feat(services): location and coursetemplate Service dummy data added

--- src/app/services/coursetemplate/coursetemplate.service.ts
+++ src/app/services/coursetemplate/coursetemplate.service.ts
@@ -4,39 +4,96 @@ import { Observable, of } from 'rxjs';
 import { CourseTemplate } from '../../models/coursetemplate/CourseTemplate';
 import { PostCourseTemplateDTO } from '../../models/coursetemplate/PostCourseTemplateDTO';
 import { Location } from '../../models/location/LocationDTO';
+import { Price } from '../../models/Price';
+import { Qualification } from '../../models/Qualification';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursetemplateService {
+  url: string = 'http://localhost:8080/api/v1/coursetemplate';
+  l1 = new Location(
+    1,
+    'west 1',
+    'Morgenstraße 2',
+    '27816',
+    'Deutschland',
+    'filler@email.com',
+    '0148131564156156',
+    'https://www.test.com/firstTest'
+  );
+  l2 = new Location(
+    2,
+    'east 2',
+    'Abendstraße',
+    '27816',
+    'Deutschland',
+    'test@email.com',
+    '0156487947787853',
+    'https://www.test.com/secondTest'
+  );
+  t1 = new CourseTemplate(
+    1,
+    'quick climb',
+    'qc34',
+    'learn to climb quick but careful',
+    2,
+    120,
+    4,
+    2,
+    [new Price('Regular', 100), new Price('Special', 80)],
+    'vor der großen Halle',
+    [
+      new Qualification(1, 'Mehrfache Erfahrung mit Gruppen'),
+      new Qualification(3, 'Erweiterte Sicherheitsmaßnahmen'),
+    ],
+    1,
+    this.l1
+  );
+  t2 = new CourseTemplate(
+    2,
+    'high climb',
+    'hc21',
+    'learn to climb higher, than the regular classes',
+    1,
+    200,
+    5,
+    2,
+    [new Price('Erweiterter Kurs', 130)],
+    'vor der großen Halle',
+    [
+      new Qualification(1, 'Mehrfache Erfahrung mit Gruppen'),
+      new Qualification(3, 'Kletterschein 2'),
+    ],
+    2,
+    this.l2
+  );
+  constructor(private http: HttpClient) {}
 
-  url:string='http://localhost:8080/api/v1/coursetemplate';
-  l1=new Location(1,'1','1','1','1','1','1','1');
-  t1=new CourseTemplate(1,'1','1','1',1,1,1,1,[],'1',[],1,this.l1);
-  t2=new CourseTemplate(2,'2','2','2',2,2,2,2,[],'2',[],2,this.l1);
-  constructor(
-    private http:HttpClient
-  ) { }
-
-  getAllCourseTemplates():Observable<CourseTemplate[]>{
+  getAllCourseTemplates(): Observable<CourseTemplate[]> {
     // return this.http.get<CourseTemplate[]>(this.url);
-    return of([this.t1,this.t2])
+    return of([this.t1, this.t2]);
   }
 
-  getCourseTemplateById(id:number):Observable<CourseTemplate>{
+  getCourseTemplateById(id: number): Observable<CourseTemplate> {
     // return this.http.get<CourseTemplate>(`${this.url}/${id}`);
-    return of(this.t1)
+    return of(this.t1);
   }
 
-  createCourseTemplate(template:PostCourseTemplateDTO):Observable<CourseTemplate>{
-    return this.http.post<CourseTemplate>(this.url,template);
+  createCourseTemplate(
+    template: PostCourseTemplateDTO
+  ): Observable<CourseTemplate> {
+    return this.http.post<CourseTemplate>(this.url, template);
   }
 
-  updateCouseTemplate(template:PostCourseTemplateDTO,id:number):Observable<CourseTemplate>{
-    return this.http.put<CourseTemplate>(`${this.url}/${id}`,template);
+  updateCouseTemplate(
+    template: PostCourseTemplateDTO,
+    id: number
+  ): Observable<CourseTemplate> {
+    return this.http.put<CourseTemplate>(`${this.url}/${id}`, template);
   }
 
-  deleteCourseTemplate(id:number):Observable<void>{
+  deleteCourseTemplate(id: number): Observable<void> {
     return this.http.delete<void>(this.url);
   }
 }
--- src/app/services/location/location.service.ts
+++ src/app/services/location/location.service.ts
@@ -2,40 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LocationDTO } from '../../models/LocationDTO';
-import { of } from 'rxjs'; 
+import { of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocationService {
+  url: string = 'http://localhost:8080/api/v1/location';
 
-  url:string="http://localhost:8080/api/v1/location"
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  getAllLocations():Observable<LocationDTO[]>{
-    let location1:LocationDTO={
-        locationId:1,
-        room:'1',
-        street:'1',
-        postCode:'1',
-        country:'1',
-        email:'1',
-        phone:'1',
-        mapsUrl:'1'
-    }
-    let location2:LocationDTO={
-      locationId:2,
-      room:'2',
-      street:'2',
-      postCode:'2',
-      country:'2',
-      email:'2',
-      phone:'2',
-      mapsUrl:'2'
-  }
-    return of([location1,location2])
+  getAllLocations(): Observable<LocationDTO[]> {
+    let location1: LocationDTO = {
+      locationId: 1,
+      room: 'west 1',
+      street: 'Morgenstraße 2',
+      postCode: '27816',
+      country: 'Deutschland',
+      email: 'filler@email.com',
+      phone: '0148131564156156',
+      mapsUrl: 'https://www.test.com/firstTest',
+    };
+    let location2: LocationDTO = {
+      locationId: 2,
+      room: 'east 2',
+      street: 'Abendstraße',
+      postCode: '27816',
+      country: 'Deutschland',
+      email: 'test@email.com',
+      phone: '0156487947787853',
+      mapsUrl: 'https://www.test.com/secondTest',
+    };
+    return of([location1, location2]);
     // return this.http.get<LocationDTO[]>(this.url);
   }
 }
-
