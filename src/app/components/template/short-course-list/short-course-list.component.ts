7ddf05b5a8c9cd2281d04bf1e66b1bff0197be7e
Leon Hefling <hefling@informationsgesellschaft.com>
2024-04-18 11:01:03 +0200

    feat(short-course-template-list): initialized short-course-template-list

---
+++ src/app/components/template/short-course-list/short-course-list.component.ts
@@ -0,0 +1,71 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CoursetemplateService } from '../../../services/coursetemplate/coursetemplate.service';
+import { Observable, of } from 'rxjs';
+import { CourseTemplate } from '../../../models/coursetemplate/CourseTemplate';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AddCourseTemplateComponent } from '../add-course-template/add-course-template.component';
+
+@Component({
+  selector: 'app-short-course-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './short-course-list.component.html',
+  styleUrl: './short-course-list.component.scss',
+})
+export class ShortCourseListComponent implements OnInit {
+  courseTemplates: Observable<CourseTemplate[]> = of([]);
+  constructor(
+    public courseTemplateService: CoursetemplateService,
+    private dialogRef: MatDialogRef<ShortCourseListComponent>,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.courseTemplates = this.courseTemplateService.getAllCourseTemplates();
+    console.log('init');
+  }
+
+  openCourseCreator(courseTemplate: CourseTemplate) {
+    this.dialog.open(AddCourseTemplateComponent, {
+      disableClose: false,
+      autoFocus: true,
+      height: '80dvh',
+      width: '60dvw',
+    });
+    console.log('open');
+  }
+
+  //         qualificationService.getAllQualifications().subscribe(data=>this.qualificationList=data);
+  //         this.priceDropwdownSetting={
+  //           singleSelection:false,
+  //           idField:'priceName',
+  //           textField:'priceValue'
+  //         }
+  //       }
+  //   //
+  //       save() {
+  //         let groupTemplate:PostGroupTemplateDto={
+  //           acronym:this.acronym.value!,
+  //           title:this.title.value!,
+  //           description:this.description.value!,
+  //           numberOfDates:this.numberOfDates.value!,
+  //           duration:this.duration.value!,
+  //           numberOfParticipant:this.numberOfParticipants.value!,
+  //           locationDTO:this.location,
+  //           meetingPoint:this.meetingPoint.value!,
+  //           priceList:this.priceList,
+  //           requiredQualificationList:this.requiredQualifications,
+  //           numberOfTrainers:this.numberOfTrainers.value!
+  //         }
+  //         console.log(groupTemplate)
+
+  //         this.dialogRef.close(JSON.stringify({
+  //           data:groupTemplate,
+  //           method: 'accept'}));
+  //       }
+
+  close() {
+    this.dialogRef.close(JSON.stringify({ method: 'cancel' }));
+  }
+}
