af9844d73599d3d797c796ee0d75fc81ab099a45
Leon Hefling <hefling@informationsgesellschaft.com>
2024-04-24 12:30:35 +0200

    feat(search-pipe): search pipe created, using FilterOption Enum

---
+++ src/app/pipes/search/search.pipe.ts
@@ -0,0 +1,47 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Course } from '../../models/course/Course';
+import { Group } from '../../models/group/Group';
+import { FilterOption } from '../../models/enums/search';
+
+@Pipe({
+  name: 'search',
+  standalone: true,
+})
+export class SearchPipe implements PipeTransform {
+  transform(
+    list: Course | Group,
+    selectedOption: FilterOption,
+    condition: string
+  ) {
+    switch (selectedOption) {
+      case FilterOption.None:
+        return list;
+      case FilterOption.Date:
+        return;
+      case FilterOption.LocationPostCode:
+        return (list as Course).location.postCode.includes(condition);
+      case FilterOption.LocationRoom:
+        return (list as Course).location.room.includes(condition);
+      case FilterOption.LocationStreet:
+        return (list as Course).location.street.includes(condition);
+      case FilterOption.PriceValue:
+        return (list as Course).prices.filter((price) => {
+          price.price = parseInt(condition);
+        });
+      case FilterOption.PriceName:
+        return (list as Course).prices.filter((price) => {
+          price.name.includes(condition);
+        });
+      case FilterOption.Qualification:
+        return (list as Course).requiredQualifications.filter((q) => {
+          q.name.includes(condition);
+        });
+      case FilterOption.Trainer:
+        return list.trainers.filter((trainer) => {
+          return trainer.lastName.includes(condition);
+        }).length
+          ? true
+          : false;
+    }
+  }
+}
