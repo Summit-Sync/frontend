73ae2ced37f84aed784a6a648f554a53aea529ed
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-05-04 13:54:13 +0200

    Modified location validation

--- src/app/models/location/Location.ts
+++ src/app/models/location/Location.ts
@@ -19,30 +19,54 @@ export class Location implements CheckboxList {
     this.displayFullName = title;
   }
 
-  validate(): boolean {
-    if (
-      !this.title ||
-      !this.street ||
-      !this.postCode ||
-      !this.country ||
-      !this.email ||
-      !this.phone ||
-      !this.mapsUrl ||
-      !this.title
-    ) {
-      console.log(
-        this.title,
-        this.street,
-        this.postCode,
-        this.country,
-        this.email,
-        this.phone,
-        this.mapsUrl,
-        this.title
-      );
-      return false;
+  validate(): boolean{
+    let result: boolean = true;
+    if(!this.locationId){
+      result = false;
+      console.error("Id darf nicht leer sein");
+      
     }
-    return true;
+    if(!this.title || this.title === ''){
+      result = false;
+      console.error("Titel darf nicht leer sein");
+      
+    }
+    if(!this.street || this.street === ''){
+      result = false;
+      console.error("Straße darf nicht leer sein");
+      
+    }
+    if(!this.postCode || this.postCode === ''){
+      result = false;
+      console.error("Postleitzahl darf nicht leer sein");
+      
+    }
+    if(!this.email || this.email === ''){
+      result = false;
+      console.error("E-Mail darf nicht leer sein");
+      
+    }
+    if(!this.phone || this.phone === ''){
+      result = false;
+      console.error("Telefonnummer darf nicht leer sein");
+      
+    }
+    if(!this.country || this.country === ''){
+      result = false;
+      console.error("Land darf nicht leer sein");
+      
+    }
+    if(!this.mapsUrl || this.mapsUrl === ''){
+      result = false;
+      console.error("Maps Url darf nicht leer sein");
+      
+    }
+    if(!this.city || this.city === ''){
+      result = false;
+      console.error("Stadt darf nicht leer sein");
+      
+    }
+    return result;
   }
 
   createPostLocation(): PostLocation {
