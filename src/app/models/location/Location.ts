a8e8430d6865eeba1451beabf0c322c2cfc88f5d
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-05-04 13:54:13 +0200

    Modified location validation

--- src/app/models/location/Location.ts
+++ src/app/models/location/Location.ts
@@ -14,20 +14,53 @@ export class Location{
     ){}
 
   validate(): boolean{
-      if (
-        !this.locationId ||
-        !this.title ||
-        !this.street ||
-        !this.postCode ||
-        !this.country ||
-        !this.email ||
-        !this.phone ||
-        !this.mapsUrl ||
-        !this.title
-      ){
-        return false;
-      }
-      return true;
+    let result: boolean = true;
+    if(!this.locationId){
+      result = false;
+      console.error("Id darf nicht leer sein");
+      
+    }
+    if(!this.title || this.title === ''){
+      result = false;
+      console.error("Titel darf nicht leer sein");
+      
+    }
+    if(!this.street || this.street === ''){
+      result = false;
+      console.error("Straße darf nicht leer sein");
+      
+    }
+    if(!this.postCode || this.postCode === ''){
+      result = false;
+      console.error("Postleitzahl darf nicht leer sein");
+      
+    }
+    if(!this.email || this.email === ''){
+      result = false;
+      console.error("E-Mail darf nicht leer sein");
+      
+    }
+    if(!this.phone || this.phone === ''){
+      result = false;
+      console.error("Telefonnummer darf nicht leer sein");
+      
+    }
+    if(!this.country || this.country === ''){
+      result = false;
+      console.error("Land darf nicht leer sein");
+      
+    }
+    if(!this.mapsUrl || this.mapsUrl === ''){
+      result = false;
+      console.error("Maps Url darf nicht leer sein");
+      
+    }
+    if(!this.city || this.city === ''){
+      result = false;
+      console.error("Stadt darf nicht leer sein");
+      
+    }
+    return result;
   }
 
   createPostLocation(): PostLocation{
