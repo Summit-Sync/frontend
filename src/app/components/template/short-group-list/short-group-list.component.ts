af2c26c095b75ca54bfd3076caff7c1b8b25c46a
Leon Hefling <hefling@informationsgesellschaft.com>
2024-06-03 11:37:06 +0200

    short-group-list now doesn't close itself before opening the group create anymore

--- src/app/components/template/short-group-list/short-group-list.component.ts
+++ src/app/components/template/short-group-list/short-group-list.component.ts
@@ -1,40 +1,70 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable, of} from "rxjs";
-import {GrouptemplateService} from "../../../services/grouptemplate/grouptemplate.service";
-import {MatDialogRef} from "@angular/material/dialog";
-import {AsyncPipe, NgForOf} from "@angular/common";
-import {GroupTemplateDTO} from "../../../models/groupTemplate/GroupTemplate";
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { GrouptemplateService } from '../../../services/grouptemplate/grouptemplate.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AsyncPipe, NgForOf } from '@angular/common';
+import { GroupTemplateDTO } from '../../../models/groupTemplate/GroupTemplate';
+import { GroupComponent } from '../../final/group/group.component';
+import { GroupService } from '../../../services/group/group.service';
+import { ToastService } from '../../../services/toast/toast.service';
 
 @Component({
   selector: 'app-short-group-list',
   standalone: true,
-  imports: [
-    AsyncPipe,
-    NgForOf
-  ],
+  imports: [AsyncPipe, NgForOf],
   templateUrl: './short-group-list.component.html',
-  styleUrl: './short-group-list.component.css'
+  styleUrl: './short-group-list.component.css',
 })
-export class ShortGroupListComponent implements OnInit{
+export class ShortGroupListComponent implements OnInit {
   groupTemplate$: Observable<GroupTemplateDTO[]> = of([]);
   //selectedTemplate: GroupTemplate;
 
   constructor(
+    private groupService: GroupService,
     private gTempService: GrouptemplateService,
-    private dialogRef: MatDialogRef<ShortGroupListComponent>
-  ) {
-  }
+    private dialogRef: MatDialogRef<ShortGroupListComponent>,
+    private dialog: MatDialog,
+    private toast: ToastService
+  ) {}
   ngOnInit(): void {
     this.groupTemplate$ = this.gTempService.getAllGroupTemplateDTOs();
     console.log('Init: Short-group-list');
   }
 
-  save(template: GroupTemplateDTO): void{
-    this.dialogRef.close(JSON.stringify({method: 'confirm', data: template}))
+  save(template: GroupTemplateDTO): void {
+    this.openCreateDialog(template);
   }
 
-  close(): void{
-    this.dialogRef.close(JSON.stringify({method: 'cancel'}))
+  openCreateDialog(template: GroupTemplateDTO) {
+    const dialogRef = this.dialog.open(GroupComponent, {
+      disableClose: true,
+      autoFocus: true,
+      height: '80dvh',
+      width: '65dvw',
+    });
+    let instance = dialogRef.componentInstance;
+    instance.template = template;
+    instance.isCreate = true;
+    dialogRef.afterClosed().subscribe((result) => {
+      const obj = JSON.parse(result);
+      if (obj.method == 'confirm-create') {
+        console.log('Dialog output: ', obj.data);
+        this.groupService.postGroup(obj.data).subscribe({
+          next: () => {
+            this.toast.showSuccessToast('Gruppe erfolgreich erstellt');
+            this.dialogRef.close(JSON.stringify({ method: 'created' }));
+          },
+          error: (err) => {
+            this.toast.showErrorToast(
+              'Erstellung der Gruppe fehlgeschlagen \n' + err
+            );
+          },
+        });
+      }
+    });
   }
 
+  close(): void {
+    this.dialogRef.close(JSON.stringify({ method: 'cancel' }));
+  }
 }
