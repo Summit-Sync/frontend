1f8b0023efe6503cc5a4ea6e196727ed652a2a99
Leon Hefling <hefling@informationsgesellschaft.com>
2024-06-04 17:08:55 +0200

    outside click now closes drop down

--- src/app/components/utilities/multi-select-dropdown/multi-select-dropdown.component.ts
+++ src/app/components/utilities/multi-select-dropdown/multi-select-dropdown.component.ts
@@ -1,97 +1,115 @@
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import {
-    Component,
-    EventEmitter,
-    Input,
-    IterableDiffer,
-    IterableDiffers,
-    OnChanges,
-    OnInit,
-    Output,
-    SimpleChange,
-    SimpleChanges,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  IterableDiffer,
+  IterableDiffers,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChange,
+  SimpleChanges,
 } from '@angular/core';
-import {CheckItemInListPipe} from '../../../pipes/checkbox/check-item-in-list.pipe';
-import {CheckboxList} from '../../../models/interfaces/CheckBoxList';
+import { CheckItemInListPipe } from '../../../pipes/checkbox/check-item-in-list.pipe';
+import { CheckboxList } from '../../../models/interfaces/CheckBoxList';
 
 @Component({
-    selector: 'app-multi-select-dropdown',
-    standalone: true,
-    imports: [CommonModule, CheckItemInListPipe],
-    templateUrl: './multi-select-dropdown.component.html',
-    styleUrl: './multi-select-dropdown.component.scss',
+  selector: 'app-multi-select-dropdown',
+  standalone: true,
+  imports: [CommonModule, CheckItemInListPipe],
+  templateUrl: './multi-select-dropdown.component.html',
+  styleUrl: './multi-select-dropdown.component.scss',
 })
 export class MultiSelectDropdownComponent implements OnInit, OnChanges {
-    @Input() allOptions: CheckboxList[];
-    @Input() selectedOptions: CheckboxList[];
-    @Input() dropdownHeadline: string;
-    @Input() allowMultiple: boolean;
-    @Input() allowedEntries: number = 999999999999999;
-    @Output() changeEvent: EventEmitter<CheckboxList[]> = new EventEmitter<CheckboxList[]>;
-    checkedBoxes: boolean[] = [];
-    showList: boolean = false;
+  @Input() allOptions: CheckboxList[];
+  @Input() selectedOptions: CheckboxList[];
+  @Input() dropdownHeadline: string;
+  @Input() allowMultiple: boolean;
+  @Input() allowedEntries: number = 999999999999999;
+  @Output() changeEvent: EventEmitter<CheckboxList[]> = new EventEmitter<
+    CheckboxList[]
+  >();
 
-    ngOnChanges(changes: SimpleChanges): void {
+  checkedBoxes: boolean[] = [];
+  showList: boolean = false;
 
-      // console.log(this.allOptions);
-        this.allOptions.forEach((aOption) =>
-            this.checkedBoxes.push(
-                this.selectedOptions.some((sOption) => {
-                    return sOption.id == aOption.id;
-                })
-            )
-        );
+  @HostListener('document:click', ['$event'])
+  clickout(event: PointerEvent) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      return;
+    } else {
+      this.showList = false;
     }
+  }
 
-    ngOnInit(): void {
-        this.allOptions.forEach((aOption) =>
-            this.checkedBoxes.push(
-                this.selectedOptions.some((sOption) => {
-                    return sOption.id == aOption.id;
-                })
-            )
-        );
-    }
+  constructor(private eRef: ElementRef) {}
 
-    test(i: number) {
-        console.log(i);
-    }
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log(this.allOptions);
+    this.allOptions.forEach((aOption) =>
+      this.checkedBoxes.push(
+        this.selectedOptions.some((sOption) => {
+          return sOption.id == aOption.id;
+        })
+      )
+    );
+  }
 
-    onCheckBoxClick(event: Event, index: number) {
-        const checkbox = event.target as HTMLInputElement;
-        const clickedObject = this.allOptions[index];
-        if (this.allowMultiple) {
-            if (this.allowedEntries > this.selectedOptions.length || !checkbox.checked) {
-                checkbox.checked
-                    ? this.addObject(clickedObject)
-                    : this.deleteObject(clickedObject);
-            } else {
-              checkbox.checked = false;
-            }
-        } else {
-            this.selectedOptions.length = 0;
-            if (checkbox.checked) {
-                this.addObject(clickedObject);
-            }
-            this.checkedBoxes.fill(false, 0, this.allOptions.length);
-            this.checkedBoxes[index] = true;
-        }
-      this.changeEvent.emit(this.selectedOptions);
-    }
+  ngOnInit(): void {
+    this.allOptions.forEach((aOption) =>
+      this.checkedBoxes.push(
+        this.selectedOptions.some((sOption) => {
+          return sOption.id == aOption.id;
+        })
+      )
+    );
+  }
 
-    deleteObject(clickedObject: CheckboxList) {
-        let tqList = this.selectedOptions;
-        tqList = tqList.filter((q) => {
-            return q.id !== clickedObject.id;
-        });
-        this.selectedOptions = tqList;
-    }
+  test(i: number) {
+    console.log(i);
+  }
 
-    addObject(clickedObject: CheckboxList) {
-        this.selectedOptions.push(clickedObject);
+  onCheckBoxClick(event: Event, index: number) {
+    const checkbox = event.target as HTMLInputElement;
+    const clickedObject = this.allOptions[index];
+    if (this.allowMultiple) {
+      if (
+        this.allowedEntries > this.selectedOptions.length ||
+        !checkbox.checked
+      ) {
+        checkbox.checked
+          ? this.addObject(clickedObject)
+          : this.deleteObject(clickedObject);
+      } else {
+        checkbox.checked = false;
+      }
+    } else {
+      this.selectedOptions.length = 0;
+      if (checkbox.checked) {
+        this.addObject(clickedObject);
+      }
+      this.checkedBoxes.fill(false, 0, this.allOptions.length);
+      this.checkedBoxes[index] = true;
     }
+    this.changeEvent.emit(this.selectedOptions);
+  }
 
-    dropDownClick() {
-        this.showList = !this.showList;
-    }
+  deleteObject(clickedObject: CheckboxList) {
+    let tqList = this.selectedOptions;
+    tqList = tqList.filter((q) => {
+      return q.id !== clickedObject.id;
+    });
+    this.selectedOptions = tqList;
+  }
+
+  addObject(clickedObject: CheckboxList) {
+    this.selectedOptions.push(clickedObject);
+  }
+
+  dropDownClick() {
+    this.showList = !this.showList;
+  }
 }
