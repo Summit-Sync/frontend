ac92445bfe7e3852ae8eba1e8f21be607efc98e3
Leon Hefling <hefling@informationsgesellschaft.com>
2024-06-06 08:57:53 +0200

    status validation fixed

--- src/app/services/validation/status/status/status-validator.service.ts
+++ src/app/services/validation/status/status/status-validator.service.ts
@@ -3,32 +3,23 @@ import { StatusDTO } from '../../../../models/status/Status';
 import { ToastService } from '../../../toast/toast.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StatusValidatorService {
-
-  constructor(
-    private toast: ToastService
-  ) { }
+  constructor(private toast: ToastService) {}
 
   validate(data: StatusDTO): boolean {
-    if(!data){
+    if (!data) {
       console.error("Status can't be empty");
       this.toast.showErrorToast('Status darf nicht leer sein');
       return false;
     }
-    let result : boolean = true;
-    if (!data.text){
+    console.log(data.text, data.statusId);
+    let result: boolean = true;
+    if (!data.text) {
       result = false;
-      console.error("Statustext darf nicht leer sein");
-      this.toast.showErrorToast("Statustext darf nicht leer sein");
-      
-    }
-    if(data.statusId < 1){
-      result = false;
-      console.error("StatusId darf nicht leer sein");
-      this.toast.showErrorToast("StatusId darf nicht leer sein");
-      
+      console.error('Statustext darf nicht leer sein');
+      this.toast.showErrorToast('Statustext darf nicht leer sein');
     }
     return result;
   }
